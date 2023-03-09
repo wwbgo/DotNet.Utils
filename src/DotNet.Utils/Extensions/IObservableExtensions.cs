0fa12f90e43a708af2cfd8b17e58c65d14b2cd91
wwb <wwbgo@qq.com>
2023-03-09 14:25:42 +0800

    fix reactive async

--- src/DotNet.Utils/Extensions/IObservableExtensions.cs
+++ src/DotNet.Utils/Extensions/IObservableExtensions.cs
@@ -5,25 +5,27 @@ namespace DotNet.Utils
 {
     public static class IObservableExtensions
     {
-        public static IObservable<TResult> SelectAsync<TSource, TResult>(this IObservable<TSource> source, Func<TSource, Task<TResult>> selector, int maxConcurrent = 1)
+        public static IObservable<TResult> SelectAsync<TSource, TResult>(this IObservable<TSource> source, Func<TSource, Task<TResult>> selector)
         {
-            return source.Select(r => Observable.FromAsync(() => selector(r))).Merge(maxConcurrent);
+            return source.Select(r =>
+            {
+                return selector(r).GetAwaiter().GetResult();
+            });
         }
-        public static IObservable<TSource> DoAsync<TSource>(this IObservable<TSource> source, Func<TSource, Task> selector, int maxConcurrent = 1)
+        public static IObservable<TSource> DoAsync<TSource>(this IObservable<TSource> source, Func<TSource, Task> selector)
+        {
+            return source.Do(r =>
+            {
+                selector(r).GetAwaiter().GetResult();
+            });
+        }
+        public static IObservable<TSource> WithAsync<TSource>(this IObservable<TSource> source, Func<TSource, Task> selector, int maxConcurrent = 1)
         {
             return source.Select(r => Observable.FromAsync(async () =>
             {
                 await selector(r);
                 return r;
             })).Merge(maxConcurrent);
-        }
-        public static IObservable<TSource> WithAsync<TSource>(this IObservable<TSource> source, Func<TSource, Task> selector)
-        {
-            return source.Select(r => Observable.FromAsync(async () =>
-            {
-                await selector(r);
-                return r;
-            })).Concat();
         }
 
         public static IObservable<T> Limit<T>(this IObservable<T> source, int maxCount = 10000, CancellationToken cancellationToken = default)
